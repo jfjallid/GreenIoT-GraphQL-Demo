42b74d241f6ddef5d893c7d6fb0a3b69a6373f04
Jimmy Fjällid <jimmy@fjallid.com>
2019-04-01 11:24:24 +0200

    Formatting. Removed static decorator from resolve methods. Added support for specifying time range to the measurements query. Removed filter_path from resolve_measurements.

--- schema.py
+++ schema.py
@@ -33,7 +33,7 @@ def _json_object_hook(d):
     return namedtuple('X', d.keys())(*d.values())
 
 
-def json2obj(data):
+def _json2obj(data):
     return json.loads(data, object_hook=_json_object_hook)
 
 
@@ -45,6 +45,8 @@ class Query(ObjectType):
         sensor_type=String(
             description='Choose sensor type from: temp, humidity, pressure, pm1, pm2_5, pm10, no2'
         ),
+        from_date=String(description="yyyy-MM-dd'T'hh:mm:ss, e.g. 2019-01-01T10:00:00"),
+        to_date=String(description="yyyy-MM-dd'T'hh:mm:ss, e.g. 2019-01-07T10:00:00"),
     )
     avgbydate = Field(
         Aggregate,
@@ -55,33 +57,35 @@ class Query(ObjectType):
         to_date=String(description="yyyy-MM-dd'T'hh:mm:ss, e.g. 2019-01-07T10:00:00"),
     )
 
-    @staticmethod
     def resolve_measurements(
-            info, sensor_name=None, amount=10, sensor_type=None, **kwargs
+            _self, info, sensor_name=None, amount=10, sensor_type=None, from_date=None, to_date=None, **kwargs
     ):
         allowed_types = ['temp', 'humidity', 'pressure', 'pm1', 'pm2_5', 'pm10', 'no2']
 
         if sensor_type not in allowed_types:
             sensor_type = None
-
         if (amount < 0) or (amount > 100):
             amount = 10
-
+        if not from_date:
+            # Default to 1 week back in time
+            from_date = (datetime.datetime.utcnow() - datetime.timedelta(days=7)).strftime('%Y-%m-%dT%H:%M:%S')
+        if not to_date:
+            to_date = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
         if sensor_name:
             if sensor_type:
-                sensor_name = sensor_name + '*' + sensor_type
+                n_query = f'{sensor_name}*{sensor_type}'
             else:
-                sensor_name = sensor_name + '*'
+                n_query = f'{sensor_name}*'
             query = {
                 'query': {
                     'bool': {
                         'filter': [
-                            {'wildcard': {'n.keyword': sensor_name}},
+                            {'wildcard': {'n.keyword': n_query}},
                             {
                                 'range': {
                                     'timestamp': {
-                                        'from': '2019-03-28T10:00:00.000',
-                                        'to': '2019-03-28T10:10:00.000',
+                                        'from': from_date,
+                                        'to': to_date,
                                     }
                                 }
                             },
@@ -89,33 +93,24 @@ class Query(ObjectType):
                     }
                 },
                 'size': amount,
-            }
-            res = es.search(
-                index='measurements-*', body=query, filter_path='hits.hits._source'
-            )['hits']['hits']
+            }  # Perhaps sort the results.
+            res = es.search(index='measurements-*', body=query)['hits']['hits']
         else:
-            res = es.search(index='measurements-*', filter_path='hits.hits._source')['hits'][
-                'hits'
-            ]
-        measurements = []
+            res = es.search(index='measurements-*')['hits']['hits']
 
-        for x in res:
-            measurements.append(json2obj(json.dumps(x['_source'])))
-        return measurements
+        return [_json2obj(json.dumps(x['_source'])) for x in res]
 
-    @staticmethod
-    def resolve_avgbydate(info, sensor_type=None, from_date=None, to_date=None, **kwargs):
+    def resolve_avgbydate(_self, info, sensor_type=None, from_date=None, to_date=None, **kwargs):
 
         allowed_types = ['temp', 'humidity', 'pressure', 'pm1', 'pm2_5', 'pm10', 'no2']
-
+        if sensor_type not in allowed_types:
+            print(f'Not allowed type: {sensor_type}, using temp instead!')  # Change to logging.info
+            sensor_type = 'temp'
         if not from_date:
             # Default to 1 week back in time
             from_date = (datetime.datetime.utcnow() - datetime.timedelta(days=7)).strftime('%Y-%m-%dT%H:%M:%S')
         if not to_date:
             to_date = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
-        if sensor_type not in allowed_types:
-            print(f'Not allowed type: {sensor_type}, using temp instead!')
-            sensor_type = 'temp'
 
         query = {
             'query': {
@@ -129,11 +124,14 @@ class Query(ObjectType):
             '_source': 'false',
             'aggs': {
                 'avg': {'avg': {'field': 'v'}},
-                'units': {'terms': {'field': 'u.keyword', 'size': '1'}},
+                'units': {'terms': {'field': 'u.keyword', 'size': '2'}},
             },
         }
         res = es.search(index='measurements-*', body=query, filter_path='aggregations')
         data = dict()
         data['avg'] = res['aggregations']['avg']['value']
+        if len(res['aggregations']['units']['buckets']) != 1:
+            raise Exception("Multiple different units in aggregation!")
+
         data['unit'] = res['aggregations']['units']['buckets'][0]['key']
-        return json2obj(json.dumps(data))
+        return _json2obj(json.dumps(data))
